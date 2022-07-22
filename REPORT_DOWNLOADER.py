4b538cc4a519af8a359707a0359c2c03c02988c8
SiHoonChris <chrisleegpock@gmail.com>
2022-07-22 18:17:52 +0900

    7th commit

--- REPORT_DOWNLOADER.py
+++ REPORT_DOWNLOADER.py
@@ -2,9 +2,12 @@
 # 1) 보유종목과 관심종목에 대한 공시/보고서를 사이트에 들어가서 일일이 확인하기 귀찮음
 # 2) 공시/보고서가 언제 올라올지도 모르는데 매일 들어가서 확인하기도 번거로움
 #    1), 2)를 해결하기 위해...
-#       a). 원하는 기간 입력하여 해당 기간에 대한 데이터 다 가져오기
-#       b). 중요한 내용(10-K, 10-Q, 사업보고서, 분기/반기 보고서, 연결재무제표 등)은 링크 다운로드 
-# * CIK, 종목코드 검색 가능하게 만들기??? / 보유 종목에 대한 내용 하나하나 기록해두기 귀찮음
+#       a). 원하는 기간을 입력하여 해당 기간에 대한 내용 다 가져오기
+#       b). 필요한 내용은 바로 확인할 수 있도록 링크 다운로드
+
+# 개선할 점 1 : 프로그램 실행 시 사용자가 원하는 날짜 입력할 수 있도록 코드 작성
+# 개선할 점 2 : us_report 실행될 때 idx 출력값이 종종 잘못나옴(1이 아니라 다른 숫자에서 시작)
+# 개선할 점 3 : time.sleep을 많이 써서 그런지, 불필요한 코드가 있어서 그런지, selenium 때문인지, 솔직히 좀 느리다.
 
 
 
@@ -22,12 +25,13 @@ import time
 import datetime
 From_date=datetime.date(2022,1,1)
 To_date=datetime.date(2022,7,21)
-# 개선1 : input 활용하여, 프로그램 작동 시 사용자가 원하는 날짜 입력할 수 있도록 수정
 
 
+# 1. 미국 주식
 def us_report(*CIKs):
 
     browser=webdriver.Chrome(options=options)
+    CNTs=0
 
     for CIK in CIKs:
         url = "https://www.sec.gov/edgar/browse/?CIK={}&owner=exclude".format(CIK)
@@ -40,12 +44,13 @@ def us_report(*CIKs):
         name=soup.find("span", attrs={"id":"name"})
         number_of_lists=soup.find("tbody").find_all("tr")
 
-        cnt=0
+        CNT=0
         for i in range(0, len(number_of_lists)):
             filing_dates=soup.find("tbody").find_all("td", attrs={"class":"sorting_1"})[i].get_text()
             if str(From_date) <= filing_dates <= str(To_date):
-                cnt+=1
-        print("[{0}]({1})".format(name.get_text(), cnt))
+                CNT+=1
+        print("[{0}]({1})".format(name.get_text(), CNT))
+        CNTs += CNT
 
         for idx, i in enumerate(range(0, len(number_of_lists)), start=1):
             form_types=soup.find("tbody").find_all("td", attrs={"class":"dtr-control"})[i].get_text()
@@ -63,16 +68,16 @@ def us_report(*CIKs):
         
         print("-"*100)
 
-    print("--- END ---")
-    print(f"종목 : {len(CIKs)}, 내용 : \n")
+    print(f"----- END ( 종목 : {len(CIKs)}, 내용 : {CNTs} ) -----\n")
     browser.quit()
-# 문제!!! : idx 출력 문제 ; GOOGL은 3번부터, LMT는 2번부터 파일 번호 출력. 왜???
 
 
 # 2. 국내 주식
 def kr_report(*CODEs):
 
     browser=webdriver.Chrome(options=options)
+    if_num=0
+    else_num=0
 
     for CODE in CODEs:
         url="https://dart.fss.or.kr/"
@@ -93,54 +98,74 @@ def kr_report(*CODEs):
 
         soup = BeautifulSoup(browser.page_source, "lxml")  # beautifulsoup으로 웹페이지 정보 가져오기
         name = soup.find("span", attrs={"class":"innerWrap"}).find("a").get_text().strip()
-        number_of_lists=soup.find("tbody").find_all("tr")
-        print("[{0}]({1})".format(name, len(number_of_lists)))  # [종목명](조건에 맞는 파일 수)
-
-        for idx, i in enumerate(range(0, len(number_of_lists)), start=1):
-            file_name=soup.find("tbody").find_all("tr")[i].find_all("td", attrs={"class":"tL"})[1].find("a")
-            file_names=file_name.get_text().strip()
-            filing_dates=soup.find("tbody").find_all("tr")[i].find_all("td")[4].get_text()
-            links=file_name["href"]
-            print("   {0:<3} {1}  ({2})".format(str(idx)+".", file_names, filing_dates))
-            if idx < 10:
-                print("      https://dart.fss.or.kr"+links)
-            elif 10 <= idx <100:
-                print("       https://dart.fss.or.kr"+links)
-            else:
-                print("        https://dart.fss.or.kr"+links)
-
         pages=soup.find("div", attrs={"class":"pageSkip"}).find_all("li")
+
         if len(pages) <= 1:
+            number_of_lists=soup.find("tbody").find_all("tr")
+            print("[{0}]({1})".format(name, len(number_of_lists)))  # [종목명](조건에 맞는 파일 수)
+
+            for idx, i in enumerate(range(0, len(number_of_lists)), start=1):
+                file_name=soup.find("tbody").find_all("tr")[i].find_all("td", attrs={"class":"tL"})[1].find("a")
+                file_names=file_name.get_text().strip()
+                filing_dates=soup.find("tbody").find_all("tr")[i].find_all("td")[4].get_text()
+                links=file_name["href"]
+                print("   {0:<3} {1}  ({2})".format(str(idx)+".", file_names, filing_dates))
+                if idx < 10:
+                    print("      https://dart.fss.or.kr"+links)
+                elif 10 <= idx < 100:
+                    print("       https://dart.fss.or.kr"+links)
+                else:
+                    print("        https://dart.fss.or.kr"+links)
+
+            if_num += len(number_of_lists)
             print("-"*100)
-            continue
+
         else:
-            for pg in range(2, len(pages)+1):
+            browser.find_element_by_xpath(f"//*[@id='psWrap']/div[2]/ul/li[{len(pages)}]/a").click()
+            time.sleep(3)
+            soup = BeautifulSoup(browser.page_source, "lxml")
+            lists_on_lastpg=soup.find("tbody").find_all("tr")
+            list_num=(len(pages)-1)*100+len(lists_on_lastpg)
+            print("[{0}]({1})".format(name, list_num))  # [종목명](조건에 맞는 파일 수)
+
+            for pg in range(1, len(pages)+1):
                 browser.find_element_by_xpath(f"//*[@id='psWrap']/div[2]/ul/li[{pg}]/a").click()
                 time.sleep(3)
-
                 soup = BeautifulSoup(browser.page_source, "lxml")
+
                 number_of_lists=soup.find("tbody").find_all("tr")
                 
-                for idx, i in enumerate(range(0, len(number_of_lists)), start=(pg-1)*100+1):
+                for idx, i in enumerate(range(0, len(number_of_lists)), start=1):
                     file_name=soup.find("tbody").find_all("tr")[i].find_all("td", attrs={"class":"tL"})[1].find("a")
                     file_names=file_name.get_text().strip()
                     filing_dates=soup.find("tbody").find_all("tr")[i].find_all("td")[4].get_text()
                     links=file_name["href"]
-                    print("   {0:<3} {1}  ({2})".format(str(idx)+".", file_names, filing_dates))
-                    print("        https://dart.fss.or.kr"+links)
+
+                    if pg <= 1:
+                        print("   {0:<3} {1}  ({2})".format(str(idx)+".", file_names, filing_dates))
+                        if idx < 10:
+                            print("      https://dart.fss.or.kr"+links)
+                        elif 10 <= idx < 100:
+                            print("       https://dart.fss.or.kr"+links)
+                        else:
+                            print("        https://dart.fss.or.kr"+links)
+
+                    else:
+                        print("   {0:<3} {1}  ({2})".format(str((pg-1)*100+idx)+".", file_names, filing_dates))
+                        print("        https://dart.fss.or.kr"+links)
+
+            else_num += list_num
             print("-"*100)
 
-    print("--- END ---")
-    print(f"종목 : {len(CODEs)}, 내용 : \n")
+    total_num = if_num + else_num
+    print(f"----- END ( 종목 : {len(CODEs)}, 내용 : {total_num} ) -----\n")
     browser.quit()
-# 문제!!! : 가져올 분량이 1페이지를 넘어갈 수도 있음(2,3, ...) / 모든 페이지의 내용 갖고 오기 위한 추가적인 코드 필요
-# 해결!!! : pages 변수 생성, pages의 갯수에 따른 조건문 만듦. selenium으로 페이지 넘기고, bs4로 페이지소스 다시 가져옴
-
 # 문제 !!!! : 종목명 옆에 가져온 파일 갯수가 실제 파일 갯수와 다름   * [종목명](파일 갯수)
 #             *** 이거 해결하면서 불필요한 코드도 줄이기 ***
+# 해결 !!!! : 종목별 웹페이지 상의 분량 다름. 페이지가 - '1 이하' 와 '1 초과'로 나눠서 조건문 작성
+#             각 조건문 상에서 출력 내용 갯수 추출, total_num으로 합산
 # 개선2 : "--- END ---" 이후에, 총합 1)몇 개의 종목과, 2) 몇 개의 파일을 가져왔는지 표시/출력
-# 해결2(50%) : 1) 리스트 내의 변수 갯수 제공
-
+# 해결2 : 1) CIKs, CODEs 내의 변수 갯수 반환(len), 2) 함수 내에 출력된 파일 갯수 세는 변수(CNTs, total_num)
 
 
 # 3. 다 끌어오기
@@ -153,6 +178,7 @@ def Report_Downloader():
     end_time=time.time()
     time_spent = end_time - start_time
     print(f"({str(round((time_spent), 4))} sec.)")
+
 
 
 if __name__=="__main__":
