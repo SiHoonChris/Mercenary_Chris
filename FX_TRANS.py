eb0b7bc1300d812f267571d93981be15db1bc5ed
SiHoonChris <chrisleegpock@gmail.com>
2022-08-25 16:57:05 +0900

    2nd commit

--- FX_TRANS.py
+++ FX_TRANS.py
@@ -10,12 +10,29 @@
 # Column Title : Date,  End-Price,  SMA-L,  SMA-UL,  SMA-LL,  Buy-Sell-Hold,  Median, Mean, Vlt.
 
 
+from openpyxl import load_workbook
+from datetime import datetime
 import pandas as pd
 
-df = pd.read_csv('USD_KRW 내역 (19.8.21~22.8.21).csv')
+
+# 데이터 가공
+file='USD_KRW 내역 (19.8.21~22.8.21).xlsx'   # 출처 : kr.investing.com
+wb = load_workbook(file)
+ws = wb.active
+
+for i in range(2, ws.max_row+1):
+    cell=ws['A{}'.format(i)].value
+    dt = datetime.fromordinal(datetime(1900, 1, 1).toordinal() + cell - 2)
+    ws['A{}'.format(i)]=dt
+
+wb.save(file)
+# 'pandas로 excel파일 끌어올 때 날짜 데이터가 5자리 숫자로 가져와짐' => 해결
+# stackoverflow 찾아봄
+
+
+# 데이터 가공 : 기본 프레임
+df = pd.read_excel(file)
 df.sort_index(ascending=False, inplace=True)
 df.reset_index(drop=True, inplace=True)
 
 print(df)
-
-# '종가' 데이터를 str로 인식함, int냐 float으로 데이터 바꿔줘야 함
