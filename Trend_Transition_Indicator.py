a0df000eea564321adebdf42cb32cb6f919b4cc1
SiHoonChris <chrisleegpock@gmail.com>
2022-08-31 14:34:09 +0900

    4th commit

--- Trend_Transition_Indicator.py
+++ Trend_Transition_Indicator.py
@@ -18,13 +18,11 @@ df['LS_B']=''
 df['ML']=''
 df['UL']=''
 df['LL']=''
-df['Cond.1']=''
-df['Cond.2']=''
-df['Cond.3']=''
+df['Cond.']=''
 
 last_idx=df.index[-1]
 for i in range(1, 26):
-    df.loc[last_idx+i] = ['', '', '', '', '', '', '', '', '', '', '', '']
+    df.loc[last_idx+i] = ['', '', '', '', '', '', '', '', '', '']
 
 # 일목균형표:전환선
 for i in df.index:
@@ -85,17 +83,19 @@ df['ML'] = pd.to_numeric(df['ML'], errors='coerce')
 df['UL'] = pd.to_numeric(df['UL'], errors='coerce')
 df['LL'] = pd.to_numeric(df['LL'], errors='coerce')
 
-# Condition 1~3
-for i in range(1, last_idx+1):
-    if df.loc[i,'Price'] > df.loc[i,'LS_A'] and df.loc[i-1,'Price'] < df.loc[i, 'LS_B'] < df.loc[i,'Price']:
-        df.loc[i,'Cond.1']='O'
-    elif df.loc[i,'Price'] > df.loc[i,'LS_B'] and df.loc[i-1,'Price'] < df.loc[i, 'LS_A'] < df.loc[i,'Price']:
-        df.loc[i,'Cond.1']='O'
-
-for i in range(1, last_idx+1):
-    if df.loc[i-1,'UL'] < df.loc[i,'UL'] and df.loc[i+1,'UL'] < df.loc[i,'UL'] and\
-    df.loc[i,'LL'] < df.loc[i,'LS_A'] and df.loc[i,'LL'] < df.loc[i,'LS_B']:
-        df.loc[i,'Cond.2']='O'
+# Condition
+idx=1
+while idx != last_idx-1:
+    if df.loc[idx,'Price'] > df.loc[idx,'LS_B'] > df.loc[idx-1,'Price'] > df.loc[idx,'LS_A'] > df.loc[idx, 'LL']:
+        df.loc[idx,'Cond.']='a'
+        idx+=1
+    else:
+        idx+=1
+        if df.loc[idx,'UL'] > df.loc[idx-1,'UL'] and df.loc[idx,'UL'] > df.loc[idx+1,'UL']:
+            df.loc[idx,'Cond.']='b'
+            idx+=1
+        else:
+            idx+=1
 
 
 print(df)
@@ -118,11 +118,10 @@ plt.fill_between(date, LS_A, LS_B, where=(LS_A >= LS_B), color='orange', alpha=0
 plt.fill_between(date, LS_A, LS_B, where=(LS_A < LS_B), color='skyblue', alpha=0.5, interpolate=True)
 
 for i in range(1, last_idx+1):
-    if df.loc[i,'Cond.1']=='O':
-        plt.axvline(x=date[i], lw=0.5, color='purple')
-for i in range(1, last_idx+1):
-    if df.loc[i,'Cond.2']=='O':
-        plt.axvline(x=date[i], lw=0.5, color='yellow')
+    if df.loc[i,'Cond.']=='a':
+        plt.axvline(x=date[i], lw=0.5, color='r')
+    elif df.loc[i,'Cond.']=='b':
+        plt.axvline(x=date[i], lw=0.5, color='b')
 
 plt.title('AAPL, 19.8.27~22.8.26, TTI-signal', fontsize=18)
 plt.xticks([df.loc[0,'Date'], df.loc[last_idx,'Date']])
@@ -132,10 +131,7 @@ plt.savefig('AAPL, 19.8.27~22.8.26, TTI-signal.png')
 
 
 # To-do
-# Condition 1~3 구현하려면 고민 좀 해야할 듯
-# Cond.1에 해당하는 값(선)이 생성되어야 Cond.2에 대한 값(선)이 생성되도록 설계
-# Cond.1에 해당하는 값(선)이 생성된 후 새로운 Cond.1이 생성되면, 이전 값 삭제
-# Cond.2는 무조건 Cond.1이 먼저 생성된 다음에 생성 가능
+# Condition => 어떻게 하면 순서대로 적용할 수 있을까??
 # 참고
 # Condition.1 - 종가가 일목균형표 구름대 상향 돌파(이 때 볼린저밴드 하단선은 일목균형표 구름 아래 위치)
 # Condition.2 - 1)이 발생한 시점 이후에, 볼린저밴드의 상단선의 n자 모양 형성(접선의 기울기=0)  
